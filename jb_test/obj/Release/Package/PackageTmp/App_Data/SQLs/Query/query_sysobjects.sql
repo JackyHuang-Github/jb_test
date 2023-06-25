92bbc795ed712bda071bc141e30b9ceb4da14e28
JACKY-WIN11\JackyHuang <jacky58.huang@gmail.com>
2023-06-25 19:09:31 +0800

    1120625-1909 表單特效功能 淡入顯示背景與訊息對話框

--- jb_test/obj/Release/Package/PackageTmp/App_Data/SQLs/Query/query_sysobjects.sql
+++ jb_test/obj/Release/Package/PackageTmp/App_Data/SQLs/Query/query_sysobjects.sql
@@ -1,20 +1,42 @@
 -- 查詢資料表
-
 SELECT *
 --SELECT COUNT(*)
 FROM sys.[sysobjects]
 WHERE 1 = 1
---AND [type] = 'U'
---AND [name] LIKE '%User%'
-AND [name] LIKE 'vi_%'
-AND
-(
-	xtype = 'U'
-	OR
-	type = 'U'
-)
+AND (xtype = 'U' OR	type = 'U')
+AND [name] LIKE 'ie%'
+--AND [name] LIKE 'vi_%'
 --AND [name] LIKE 'Z_%'
+--AND [name] LIKE '%User%'
+--AND [name] != 'iepb00h'		-- 排除幣別基本資料檔
 ORDER BY [name]
+
+
+/*
+
+SELECT COUNT(*)
+FROM sys.[sysobjects]
+WHERE 1 = 1
+AND [name] != 'iepb00h'		-- 排除幣別基本資料檔
+AND (xtype = 'U' OR	type = 'U')
+--AND [name] LIKE 'ie%'
+--AND [name] LIKE 'vi_%'
+--AND [name] LIKE 'Z_%'
+--AND [name] LIKE '%User%'
+
+SELECT *
+FROM sys.[sysobjects]
+WHERE 1 = 1
+AND (xtype = 'U' OR	type = 'U')
+AND LEN([name]) IN
+(
+	SELECT MAX(LEN([name]))
+	FROM sys.[sysobjects]
+	WHERE 1 = 1
+	AND (xtype = 'U' OR	type = 'U')
+)
+
+*/
 
 /*
 
@@ -24,11 +46,6 @@ SET
 	type = 'V'
 WHERE 1 = 1
 AND [name] LIKE 'vi_%'
-AND
-(
-	xtype = 'U'
-	OR
-	type = 'U'
-)
+AND (xtype = 'U' OR	type = 'U')
 
 */
